2501fd587c37431f1770ef370f8516a2947d6066
PraditaAdiantama <aditadiantama@gmail.com>
2024-02-07 11:03:49 +0700

    fix: leave message

--- src/events/guildMemberRemove/stats.js
+++ src/events/guildMemberRemove/stats.js
@@ -1,5 +1,7 @@
 const leave = require("../../utils/leave")
 
-module.exports = (member, client) => {
-    leave(member, client)
+module.exports = async (member, client) => {
+  client.channels.cache
+    .get(process.env.WELCOME_CHANNEL_ID)
+    .send({ files: [await leave(member, client)] });
 }
