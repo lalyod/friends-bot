a8d696a63022145352cb5f464fbba9344225c771
PraditaAdiantama <aditadiantama@gmail.com>
2024-02-07 11:07:17 +0700

    fix: not sure

--- src/validations/dev-only.js
+++ src/validations/dev-only.js
@@ -1,8 +1,10 @@
-module.exports = (interaction,commandObj) => {
-    if(commandObj.devOnly){
-        if(interaction.member.id !== '793991682310799360'){
-            interaction.reply('Command ini hanya bisa di gunakan developer')
-            return true;
-        }
+module.exports = (interaction, commandObj) => {
+  if (commandObj.devOnly) {
+    if (interaction.member.id == "454505144068079616" || interaction.member.id == "793991682310799360") {
+      return false;  
+    }else{
+      interaction.reply('Command ini hanya bisa digunakan developer atau admin');
+      return true;
     }
-}
+  }
+};
