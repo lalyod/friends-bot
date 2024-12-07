7eab3c1727ed813009d1b70abd5cafb461a9cb3b
PraditaAdiantama <mpaproject307@gmail.com>
2024-12-07 20:07:53 +0700

    refactor: change welcome message

--- src/commands/admin/simulate-join.js
+++ src/commands/admin/simulate-join.js
@@ -1,16 +1,30 @@
-const {SlashCommandBuilder} = require('discord.js');
+const { SlashCommandBuilder } = require('discord.js')
 
-module.exports =  {
+module.exports = {
   data: new SlashCommandBuilder()
     .setName('simulate-join')
     .setDescription('Simulates a user joining the server.')
-    .addUserOption(option => option.setName('user')
-                  .setDescription('target-user')),
-  run: async ({interaction, client}) => {
-    client.emit('guildMemberAdd', interaction.guild.members
-                .cache.get(interaction.options.get('user').value));
+    .addUserOption((option) =>
+      option.setName('user').setDescription('target-user').setRequired(true)
+    ),
+  run: async ({ interaction, client }) => {
+    try {
+      const user = interaction.options.get('user').value
+      const member = interaction.guild.members.cache.get(user)
+      client.emit('guildMemberAdd', member)
 
-    interaction
-      .reply({content: 'Berhasil mengsimulasi', emepheral: true});
-  }
+      await interaction.reply({
+        content: 'Berhasil mengsimulasi',
+        emepheral: true,
+      })
+    } catch (err) {
+      const dev = await client.users.fetch(process.env.BOT_DEV_ID)
+      dev.send({ content: `\`${err.message}\`` })
+
+      await interaction.reply({
+        content: 'Sorry sedang terjadi masalah, silahkan contact developer',
+      })
+    }
+  },
 }
+
--- src/events/guildMemberAdd/welcome.js
+++ src/events/guildMemberAdd/welcome.js
@@ -1,7 +1,11 @@
-const welcome = require('../../utils/welcome')
-
+/**
+ * @param {import('discord.js').Client} client
+ */
 module.exports = async (member, client) => {
-  client.channels.cache
-    .get(process.env.WELCOME_CHANNEL_ID)
-    .send({ files: [await welcome(member, client)] });
+  const channel = client.channels.cache.get(process.env.WELCOME_CHANNEL_ID)
+
+  channel.send({
+    content: `Selamat datang ${member} aku adalah bot pembantu. kalau butuh bantuan gunain \`/help\` aja. semoga kamu betah disini`,
+  })
 }
+
