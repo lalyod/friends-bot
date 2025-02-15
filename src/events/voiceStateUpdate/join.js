79d1d7d33b6ce6b07128a560dc5a8bc885b57ef3
PraditaAdiantama <mpaproject307@gmail.com>
2025-02-15 08:26:06 +0700

    fix voice bug

--- src/events/voiceStateUpdate/join.js
+++ src/events/voiceStateUpdate/join.js
@@ -7,16 +7,20 @@ const Voice = require('../../models/voice')
  * @param {Client} client
  */
 module.exports = async (oldState, newState, client) => {
-  if (oldState?.channel && !newState?.channel) return
-  const member = oldState?.member ?? newState?.member
+  // ensure that this is join voice event
+  if (oldState.channelId !== newState.channelId) {
+    // prevent mute and unmute trigger this event
+    if (!oldState.channelId && newState.channelId) {
+      const member = oldState?.member ?? newState?.member
 
-  const voice = await Voice.findOne({ user_id: member.id })
+      const voice = await Voice.findOne({ user_id: member.id })
 
-  if (voice) {
-    await voice.deleteOne({ user_id: member.id })
-    return
+      if (voice) {
+        await voice.deleteOne({ user_id: member.id })
+      }
+
+      const newVoice = new Voice({ user_id: member.id })
+      await newVoice.save()
+    }
   }
-
-  const newVoice = new Voice({ user_id: member.id })
-  await newVoice.save()
 }
--- src/events/voiceStateUpdate/leave.js
+++ src/events/voiceStateUpdate/leave.js
@@ -2,17 +2,20 @@ const Voice = require('../../models/voice')
 const levelUp = require('../../utils/level-up')
 
 module.exports = async (oldState, newState, client) => {
-  if (!oldState?.channel && newState?.channel) return
-  const member = oldState?.member ?? newState?.member
+  if (oldState.channel && !newState.channel) {
+    const member = oldState?.member ?? newState?.member
 
-  const voice = await Voice.findOneAndDelete({ user_id: member.id })
+    const voice = await Voice.findOne({ user_id: member.id })
 
-  if(!voice) return
+    if (!voice) {
+      return
+    }
 
-  const timeElapse = Math.round((Date.now() - voice.createdAt) / 1000)
+    const timeElapse = Math.round((Date.now() - voice.createdAt) / 1000)
 
-  if (timeElapse > 60) {
-    const xp = 5 * Math.round(timeElapse / 60)
-    await levelUp(member.id, xp, client)
+    if (timeElapse > 60) {
+      const xp = 5 * Math.round(timeElapse / 60)
+      await levelUp(member.id, xp, client)
+    }
   }
 }
