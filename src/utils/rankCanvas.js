edfb001efbd2c7fa53f44af1953541ddacbdae82
PraditaAdiantama <mpaproject307@gmail.com>
2025-06-28 10:46:02 +0700

    improve rank canvas

--- src/utils/rankCanvas.js
+++ src/utils/rankCanvas.js
@@ -6,7 +6,7 @@ module.exports = async (levels, members) => {
         'Poppins'
     )
 
-    const canvas = createCanvas(510, 408)
+    const canvas = createCanvas(510, 360)
     const ctx = canvas.getContext('2d')
 
     ctx.fillStyle = '#18191C'
@@ -25,30 +25,52 @@ module.exports = async (levels, members) => {
             member.displayAvatarURL({ extension: 'jpg' })
         )
 
+        ctx.beginPath()
+        ctx.strokeStyle = 'black'
+        ctx.lineWidth = 3
+        ctx.arc(80, cardY + 30, 23, 0, Math.PI * 2, true)
+        ctx.stroke()
+
         const xpProgress = levels[i].xp / (levels[i].level * 10)
         ctx.beginPath()
-        ctx.strokeStyle = 'green'
+        if (xpProgress >= 0.5) {
+            ctx.strokeStyle = '#63F58C'
+        } else {
+            ctx.strokeStyle = '#FDB766'
+        }
         ctx.lineWidth = 3
-        ctx.arc(50, cardY + 30, 23, 0, xpProgress * (-Math.PI * 2), true)
+        ctx.arc(80, cardY + 30, 23, 0, xpProgress * (-Math.PI * 2), true)
         ctx.stroke()
 
         ctx.save()
         ctx.beginPath()
-        ctx.arc(50, cardY + 30, 20, 0, Math.PI * 2, true)
+        ctx.arc(80, cardY + 30, 20, 0, Math.PI * 2, true)
         ctx.clip()
-        ctx.drawImage(avatar, 30, cardY + 10, 40, 40)
+        ctx.drawImage(avatar, 60, cardY + 10, 40, 40)
         ctx.restore()
         ctx.beginPath()
 
         ctx.font = '100 16px Poppins'
+        if (i == 0) {
+            ctx.fillStyle = '#FFB01E'
+        } else if (i == 1) {
+            ctx.fillStyle = '#6093BA'
+        } else if (i == 2) {
+            ctx.fillStyle = '#C66E04'
+        } else {
+            ctx.fillStyle = '#ffffff'
+        }
+        ctx.fillText(`#${i + 1}`, 23, cardY + 35)
+
+        ctx.font = '100 16px Poppins'
         ctx.fillStyle = '#ffffff'
-        ctx.fillText(member.displayName, 30 + 40 + 15, cardY + 35)
+        ctx.fillText(member.displayName, 30 + 40 + 15 + 30, cardY + 35)
 
         ctx.font = '100 16px Poppins'
         ctx.fillStyle = '#ffffff'
         ctx.fillText(
             levels[i].level.toString(),
-            canvas.width - 30 - 20,
+            canvas.width - 30 - 20 - 10,
             cardY + 35
         )
 
